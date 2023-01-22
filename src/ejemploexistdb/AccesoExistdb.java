3a0041496af81053c478b0de0e1d3a1f9cf8a9ed
oscar <ovinan@gmail.com>
2023-01-22 22:14:37 +0100

    Commit inicial

---
+++ src/ejemploexistdb/AccesoExistdb.java
@@ -0,0 +1,42 @@
+package ejemploexistdb;
+
+import org.xmldb.api.base.*;
+import org.xmldb.api.modules.*;
+import org.xmldb.api.*;
+
+/**
+ * Obtencion del contenido del documento de una coleccion de la BD en eXist DB
+ * PREREQUISITOS: 
+ * (1) Importar en el proyecto las librerias:
+ * XMLDB-API, XMLRPC-CLIENT, XMLRPC-COMMON,LOG4J-API, LOG4J-CORE, WS-COMMONS, COMMONS-IO
+ * y EXIST-CORE.
+ * (2) Crear una coleccion llamada PRUEBAS, con un documento libros.xml
+ * 
+ * @author oscar
+ */
+public class AccesoExistdb {
+        protected static String DRIVER = "org.exist.xmldb.DatabaseImpl"; 
+        protected static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc"; 
+        protected static String collectionPath = "/db/pruebas/"; 
+        protected static String resourceName = "libros.xml"; 
+ 
+        public static void main(String[] args) throws Exception { 
+ 
+                // initialize database driver 
+                Class cl = Class.forName(DRIVER); 
+                Database database = (Database) cl.newInstance(); 
+                DatabaseManager.registerDatabase(database); 
+ 
+                // get the collection 
+                Collection col = DatabaseManager.getCollection(URI + collectionPath); 
+ 
+                // get the content of a document 
+                System.out.println("Get the content of " + resourceName); 
+                XMLResource res = (XMLResource) col.getResource(resourceName); 
+                if (res == null) { 
+                        System.out.println("document not found!"); 
+                } else { 
+                        System.out.println(res.getContent()); 
+                } 
+        }     
+}
